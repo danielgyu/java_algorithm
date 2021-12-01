5d9718da38db32c900e5598b82ffae59da013a69
kungyu <hismayfly@gmail.com>
2021-12-02 00:09:46 +0900

    two dimensional arrays

---
+++ data_structures/array/TwoDimensionalArray.java
@@ -0,0 +1,10 @@
+class TwoDimensionalArray {
+    public static void main(String[] argv) {
+	int rows = 3;
+	int columns = 4;
+
+	int[][] myMatrix = new int[rows][columns];
+
+	System.out.println(myMatrix[0][2]);
+    }
+}
